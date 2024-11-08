7e27e289114c375b1986a0da31718e313ddfab31
신희성 <vvwv3092@gmail.com>
2024-11-08 10:21:02 +0900

    update: 핸들러 반환 타입 변경

--- src/main/java/gsm/gsmjava/global/security/handler/CustomAccessDeniedHandler.java
+++ src/main/java/gsm/gsmjava/global/security/handler/CustomAccessDeniedHandler.java
@@ -1,10 +1,13 @@
 package gsm.gsmjava.global.security.handler;
 
-import jakarta.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gsm.gsmjava.global.error.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -12,11 +15,21 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         log.info("========== ACCESS DENIED ==========");
-        response.sendError(HttpStatus.FORBIDDEN.value(), "요청을 수행할 수 있는 권한이 없습니다.");
+
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(
+                new ErrorResponse("요청을 수행할 수 있는 권한이 없습니다.")
+        ));
     }
 }
